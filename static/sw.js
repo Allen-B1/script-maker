143d3f4f4d816dd621c71cf5b9e9bb2dcfd2a8f0
Allen B <pingguo.ny@gmail.com>
2023-08-13 12:08:16 -0400

    caching

---
+++ static/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", (event) => {
+    console.log("installed");
+});
+
+self.addEventListener("fetch", (event) => {
+    console.log("fetch event");
+    if (event.request.url.startsWith("https://script.bloodontheclocktower.com") || event.request.url.startsWith("https://www.bloodstar.xyz")) {    
+        let f = (async () => {
+            let resp = await caches.match(event.request);
+            if (!resp) {
+                let [cache, resp] = await Promise.all([caches.open("v1"), fetch(event.request.url)]);
+                if (!resp.ok) {
+                    throw new TypeError("bad response status");
+                }
+                cache.put(event.request, resp.clone());
+                return resp.clone();
+            }
+            return resp.clone();
+        });
+        let promise = f();
+        if (!(promise instanceof Promise)) {
+            throw new Error("not promise");
+        }
+        event.respondWith(promise);
+    }
+});
