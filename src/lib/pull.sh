03970377d7865789ca55447eda65675d6b109d5a
Allen B <pingguo.ny@gmail.com>
2023-08-17 23:46:14 -0400

    update roles

---
+++ src/lib/pull.sh
@@ -0,0 +1,4 @@
+curl "https://raw.githubusercontent.com/SansCipher/bootlegger/main/bootleggers_anthology.json" > src/lib/anth.json
+curl "https://script.bloodontheclocktower.com/data/jinx.json" > src/lib/base-jinx.json
+curl "https://script.bloodontheclocktower.com/data/roles.json" > src/lib/base-official.json
+curl "https://clocktower.patters.live/roles" > src/lib/base-patters.json
